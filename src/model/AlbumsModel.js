ad32ef5a81e85e55ce8be0e3acec57dc07f4064b
Alex Viktorov <viktorovaleksandr7@gmail.com>
2021-03-22 14:28:29 +0200

    fix

--- src/model/AlbumsModel.js
+++ src/model/AlbumsModel.js
@@ -1,5 +1,9 @@
 export class AlbumsModel {
 
+  constructor() {
+		this.albums = [];
+  }
+  
   async getAlbums() {
     return fetch('https://jsonplaceholder.typicode.com/albums')
     .then((response) => response.json())
--- src/model/PhotosModel.js
+++ src/model/PhotosModel.js
@@ -1,5 +1,9 @@
 export class PhotosModel {
 	
+  constructor() {
+		this.photos = [];
+  }
+
 	async getPhotos(id) {
     return fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${id}`)
     .then((response) =>   response.json())
